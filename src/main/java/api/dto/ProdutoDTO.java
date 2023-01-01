988cd15bd9e7c12583e93fd7297ef4bb30c8cf90
Marcos Venicios E. Chaves <markusvynycyus@hotmail.com>
2023-01-01 13:14:27 -0300

    7º Commit - Criando CategoriaController e CategoriaDTO

---
+++ src/main/java/api/dto/ProdutoDTO.java
@@ -0,0 +1,18 @@
+package api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProdutoDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private Boolean ativo;
+
+}
