5773fa8c80eb8ac0938fa2b25a54101ff7f3660f
markusvynycyus <markusvynycyus@hotmail.com>
2023-01-03 08:46:08 -0300

    21ºCommit - Criando UsuarioModelAssembler

---
+++ src/main/java/api/assembler/UsuarioModelAssembler.java
@@ -0,0 +1,30 @@
+package api.assembler;
+
+import api.dto.ProdutoDTO;
+import api.dto.UsuarioDTO;
+import domain.model.Produto;
+import domain.model.Usuario;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UsuarioModelAssembler {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UsuarioDTO toModel(Usuario usuario) {
+
+        return modelMapper.map(usuario, UsuarioDTO.class);
+    }
+
+    public List<UsuarioDTO> toCollectionModel(List<Usuario> usuarios) {
+        return usuarios.stream()
+                .map(usuario -> toModel(usuario))
+                .collect(Collectors.toList());
+    }
+
+}
