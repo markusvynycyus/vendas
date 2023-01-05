1a8033db3b5e3f685c109622c4b11b1f8e7d042b
markusvynycyus <markusvynycyus@hotmail.com>
2023-01-05 11:15:14 -0300

    24ºCommit - Criando PedidoInput, ItemPedidoInput.java

---
+++ src/main/java/api/dto/input/ItemPedidoInput.java
@@ -0,0 +1,25 @@
+package api.dto.input;
+
+
+
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.PositiveOrZero;
+
+@Setter
+@Getter
+
+public class ItemPedidoInput {
+
+    @NotNull
+    private Long produtoId;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer quantidade;
+
+
+    private String observacao;
+}
---
+++ src/main/java/api/dto/input/PedidoInput.java
@@ -0,0 +1,12 @@
+package api.dto.input;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PedidoInput {
+
+
+}
